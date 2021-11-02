d8205ac08fd4853ff0b8b2d467ee53a083208f90
Sarthak Mahajan <sarthak.mahajan54@gmail.com>
2021-11-02 12:35:23 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAQYuy5gNXSFwYXbaoAHgun6kSEwbP6y0A",
+	authDomain: "whatsapp-mern-10b42.firebaseapp.com",
+	projectId: "whatsapp-mern-10b42",
+	storageBucket: "whatsapp-mern-10b42.appspot.com",
+	messagingSenderId: "299536095141",
+	appId: "1:299536095141:web:d56ce38b566ea394e87a4c"
+      };
